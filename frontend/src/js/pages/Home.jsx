11de379add3bfba5f289be6c893f38ba664bdbff
DerianCordobaPerez <derianunanleon@gmail.com>
2021-10-23 21:49:39 -0600

    Creacion del componente MenuAppBar

--- frontend/src/js/pages/Home.jsx
+++ frontend/src/js/pages/Home.jsx
@@ -1,3 +1,10 @@
+import {MenuAppBar} from '../components/menuAppBar.jsx'
+
 export function Home() {
-  return <h1>This is the Home Page!</h1>
+  return (
+    <>
+      <MenuAppBar title="Menu principal" variant="h6" />
+      <h1>This home page!</h1>
+    </>
+  )
 }
